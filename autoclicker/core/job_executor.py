e4b227ceaa515fa29ee48434808eb9e880b4cb4a
Amin <minhdinhkhoi@gmail.com>
2025-12-16 01:50:40 +0700

    Update job_executor.py

--- autoclicker/core/job_executor.py
+++ autoclicker/core/job_executor.py
@@ -373,19 +373,3 @@ class JobExecutor:
             if job_object and hasattr(job_object, 'running'):
                 job_object.running = False
             logger.info(f"Job '{self._job.name}': Execution loop finished. Final run count: {self._current_run_count}.")
-
-# Auto Clicker Enhanced
-# Copyright (C) <2025> <Đinh Khởi Minh>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
