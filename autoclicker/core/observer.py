ac4bd196d0ca6ad7a7e7a1c9a21cc08d17a606fb
Amin <minhdinhkhoi@gmail.com>
2025-12-16 01:51:45 +0700

    Remove license comments from observer.py
    Removed license comments from the observer.py file.

--- autoclicker/core/observer.py
+++ autoclicker/core/observer.py
@@ -300,19 +300,3 @@ class Observer:
     
     def destroy(self) -> None:
         self.stop(wait=True)
-
-# Auto Clicker Enhanced
-# Copyright (C) <2025> <Đinh Khởi Minh>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
