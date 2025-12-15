7ebf935fa48bf5620cd8519eb1f5e19e3cb3f28c
Amin <minhdinhkhoi@gmail.com>
2025-12-16 01:50:25 +0700

    Remove license comments from job.py
    Removed license comments from job.py file.

--- autoclicker/core/job.py
+++ autoclicker/core/job.py
@@ -183,19 +183,3 @@ class Job:
         return (f"Job(name={repr(self.name)}, actions={actions_repr}, hotkey={repr(self.hotkey)}, "
                 f"stop_key={repr(self.stop_key)}, enabled={repr(self.enabled)}, run_condition={run_condition_repr}, "
                 f"running={repr(self.running)}, params={repr(self.params)})")
-
-# Auto Clicker Enhanced
-# Copyright (C) <2025> <Đinh Khởi Minh>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
