773f3a477fdcd96489a84584045c30a21c61cb3e
Amin <minhdinhkhoi@gmail.com>
2025-12-16 01:52:42 +0700

    Remove license comments from trigger_edit.py
    Removed license comments from trigger_edit.py.

--- autoclicker/gui/trigger_edit.py
+++ autoclicker/gui/trigger_edit.py
@@ -483,19 +483,3 @@ class TriggerEdit(ttk.Frame):
              try: self._action_edit_dialog.destroy()
              except Exception: pass
         super().destroy()
-
-# Auto Clicker Enhanced
-# Copyright (C) <2025> <Đinh Khởi Minh>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
