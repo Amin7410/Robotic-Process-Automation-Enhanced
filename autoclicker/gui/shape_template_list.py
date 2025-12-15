835047c47995ba2e40d37ed2e076fb807c4e0a33
Amin <minhdinhkhoi@gmail.com>
2025-12-16 01:53:20 +0700

    Remove license comments and adjust refresh method

--- autoclicker/gui/shape_template_list.py
+++ autoclicker/gui/shape_template_list.py
@@ -129,19 +129,3 @@ class ShapeTemplateList(ttk.Frame):
     def refresh_template_list(self):
         logger.info("Refreshing shape template list display.")
         self._populate_template_list()
-
-# Auto Clicker Enhanced
-# Copyright (C) <2025> <Đinh Khởi Minh>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
