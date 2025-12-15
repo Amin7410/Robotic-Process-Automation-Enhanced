20afac48b1d190334fd7cfc273ff37a6cb756d5e
Amin <minhdinhkhoi@gmail.com>
2025-12-16 01:50:57 +0700

    Remove copyright and license comments
    Removed copyright notice and license information from job_manager.py.

--- autoclicker/core/job_manager.py
+++ autoclicker/core/job_manager.py
@@ -740,19 +740,5 @@ class JobManager:
                     self._bind_all_keys() 
                 else:
                     logger.debug("JobManager: Hook state change (released), but JobManager not in 'globally recording' state. No action on bindings.")
-
-# Auto Clicker Enhanced
-# Copyright (C) <2025> <Đinh Khởi Minh>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
